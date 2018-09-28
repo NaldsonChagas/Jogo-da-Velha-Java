4a46f88d177932f2ced485bfb820f92f8f41bb02
Naldson Chagas <naldson.bc@gmail.com>
2018-09-28 00:36:02 -0300

    First commit

---
+++ src/br/com/naldson/jogodavelha/testejogo/TesteJogo.java
@@ -0,0 +1,11 @@
+package br.com.naldson.jogodavelha.testejogo;
+
+import br.com.naldson.jogodavelha.jogo.BaseJogo;
+
+public class TesteJogo {
+	public static void main(String[] args) {
+		BaseJogo jogo = new BaseJogo();
+		
+		jogo.inicia();
+	}
+}
